df22a49b60647a90030b35d7831be2b5540f5adc
Mochan <ryohei.s@fantamstick.jp>
2022-03-31 17:12:25 +0900

    update.

--- script/index.ts
+++ script/index.ts
@@ -1,3 +1,33 @@
-$(() => {
-  console.log("puyo");
-});
+const callback = () => {
+  alert();
+}
+
+window.onload = () => {
+
+  const elements: HTMLElement[] = [];
+
+  let el: any = document.getElementById("hoge1");
+  elements.push(el);
+
+  el = document.getElementById("hoge2");
+  elements.push(el);
+
+  const ctx: any = el.getContext("2d");
+  const chara = new Image();
+  chara.src = "../img/sample_canvas.jpg";  // 画像のURLを指定
+  chara.onload = () => {
+    ctx.drawImage(chara, 0, 0);
+  };
+
+  el = document.getElementById("hoge3");
+  elements.push(el);
+
+  for (const el of elements) {
+    el.oncontextmenu =  () => {
+      return false;
+    };
+  }
+
+
+
+};
