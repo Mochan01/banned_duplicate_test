5448103804c819e119b409093f4abb6272121d7e
Mochan <ryohei.s@fantamstick.jp>
2022-06-02 19:41:54 +0900

    hoge

--- script/index.ts
+++ script/index.ts
@@ -1,33 +1,13 @@
-const callback = () => {
-  alert();
-}
+
 
 window.onload = () => {
-
-  const elements: HTMLElement[] = [];
-
-  let el: any = document.getElementById("hoge1");
-  elements.push(el);
-
-  el = document.getElementById("hoge2");
-  elements.push(el);
-
-  const ctx: any = el.getContext("2d");
-  const chara = new Image();
-  chara.src = "https://mochan01.github.io/banned_duplicate_test/img/sample_canvas.jpg";  // 画像のURLを指定
-  chara.onload = () => {
-    ctx.drawImage(chara, 0, 0);
-  };
-
-  el = document.getElementById("hoge3");
-  elements.push(el);
-
-  for (const el of elements) {
-    el.oncontextmenu =  () => {
-      return false;
-    };
-  }
-
-
-
+  const button = document.getElementById("hoge");
+  button?.addEventListener("click", () => {
+    const now = Date.now();
+    // @ts-ignore
+    window.gtag('event', 'aaa', {
+      'event_category' : 'bbb',
+      'event_label' : now
+    });
+  });
 };
