f22b1425e63470d4d65291428331f5470a975d9c
Mochan <ryohei.s@fantamstick.jp>
2022-06-16 20:56:06 +0900

    s

--- script/index.ts
+++ script/index.ts
@@ -1,9 +1,17 @@
 
 
 window.onload = () => {
+  /*
   const el = document.getElementById("puyo");
   setTimeout(() => {
     if (!el) return;
     el.innerText = "hogehogehoge";
   }, 3000)
+  */
+
+  // @ts-ignore
+  dataLayer.push({
+    'event': 'event_puyo',
+    'puyo': 'hogehogehoge'
+  });
 };
